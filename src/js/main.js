6a85cd36233369a5a3f439aa67f9a2b1bf8f3e40
Sofia <itssonyamisyura@gmail.com>
2025-11-25 16:18:22 +0100

    реализован функционал модальных окон

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "./slider";
+import modals from "./modules/modals";
+
+// скрипты начинают выполнятся когда готова dom структура
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+});
+
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+import $ from 'jquery';
+import 'slick-carousel';
+
+$(function ()  {
     $('.glazing_slider').slick({
         infinite: true,
         slidesToShow: 5,
